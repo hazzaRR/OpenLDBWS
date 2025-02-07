d78ed12508f755b3f9e5c2eb73646198bf30fc5c
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-02-07 17:38:04 +0000

    Update calling point to have all properties based on docs

--- Entities/CallingPoint.cs
+++ Entities/CallingPoint.cs
@@ -17,9 +17,37 @@ public class CallingPoint
     [XmlElement("et")]
     public string Et { get; set; } = "";
 
+    [XmlElement("at")]
+    public string At { get; set; } = "";
+
+    [XmlElement("isCancelled")]
+    public bool? IsCancelled { get; set; }
+    
     [XmlElement("length")]
     public int Length { get; set; }
 
+    [XmlElement("detachFront")]
+    public bool? DetachFront { get; set;}
+
     [XmlElement("formation")]
     public Formation? Formation { get; set; }
+
+    [XmlElement("adhocAlerts")]
+    public List<string> AdhocAlerts { get; set; } = [];
+
+    [XmlElement("cancelReason")]
+    public string? CancelReason { get; set; }
+    [XmlElement("delayReason")]
+    public string? DelayReason { get; set; }
+
+    [XmlElement("affectedByDiversion")]
+    public string? AffectedByDiversion { get; set; }
+
+    [XmlElement("rerouteDelay")]
+    public int? RerouteDelay { get; set; }
+
+    [XmlElement("uncertainty")]
+    public Uncertainty? Uncertainty { get; set; }
+
+
 }
