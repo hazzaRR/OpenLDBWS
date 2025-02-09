0d37823ddad1f0c7593531c91782c93f0bd17c38
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-02-09 14:52:22 +0000

    StationBoard and StationBoardWith details entities

--- Entities/StationBoard/StationBoardWithDetails.cs
+++ Entities/StationBoard/StationBoardWithDetails.cs
@@ -4,31 +4,17 @@ namespace TrainDepartureBoard.Entities;
 
 
 [XmlRoot(ElementName = "GetStationBoardResult")]
-public class StationBoardWithDetails
+public class StationBoardWithDetails: StationBoard
 {
-        [XmlElement(ElementName = "generatedAt", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public string GeneratedAt { get; set; } = "";
-
-        [XmlElement(ElementName = "locationName", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public string LocationName { get; set; } = "";
-
-        [XmlElement(ElementName = "crs", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public string Crs { get; set; } = "";
-
-        [XmlArray(ElementName = "nrccMessages", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        [XmlArrayItem(ElementName = "message", Namespace = "http://thalesgroup.com/RTTI/2012-01-13/ldb/types")]
-        public List<string> NrccMessages { get; set; } = [];
-
-        [XmlElement(ElementName = "filterLocationName", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public string FilterLocationName { get; set; } = "";
-
-        [XmlElement(ElementName = "filtercrs", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public string FilterCrs { get; set; } = "";
-
-        [XmlElement(ElementName = "platformAvailable", Namespace = "http://thalesgroup.com/RTTI/2015-11-27/ldb/types")]
-        public bool PlatformAvailable { get; set; } = false;
-
         [XmlArray(ElementName = "trainServices", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
         [XmlArrayItem(ElementName = "service", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
-        public List<Service> TrainServices { get; set; } = [];
+        public new List<ServiceWithCallingPoints> TrainServices { get; set; } = [];
+
+        [XmlArray(ElementName = "busServices", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
+        [XmlArrayItem(ElementName = "service", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
+        public new List<ServiceWithCallingPoints> BusServices { get; set; } = [];
+
+        [XmlArray(ElementName = "ferryServices", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
+        [XmlArrayItem(ElementName = "service", Namespace = "http://thalesgroup.com/RTTI/2021-11-01/ldb/types")]
+        public new List<ServiceWithCallingPoints> FerryServices { get; set; } = [];
 }
