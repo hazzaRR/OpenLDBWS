53adce0735f29fac573109d0178f09868774eccd
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-02-09 15:11:00 +0000

    ServiceDiversion class implementation

--- Entities/Service/ServiceDiversion.cs
+++ Entities/Service/ServiceDiversion.cs
@@ -1,6 +1,22 @@
+using System.Xml.Serialization;
+
 namespace TrainDepartureBoard.Entities;
 
 public class ServiceDiversion
 {
-    
+    [XmlElement("reason")]
+
+    public string Reason { get; set;} = "";
+
+    [XmlElement("divertedVia")]
+
+    public string DivertedVia { get; set; } = "";
+
+    [XmlElement("between")]
+
+    public ServiceDiversionBetween? Between { get; set;}
+
+    [XmlElement("reRouteDelay")]
+    public int ReRouteDelay { get; set;}
+
 }
