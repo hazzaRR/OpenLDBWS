25d3798a0e11d26dc736e3e9e26eb1f5ae9f07f2
Harry Redman <68497855+hazzaRR@users.noreply.github.com>
2025-02-12 17:08:23 +0000

    added all the interface methods needed for interacting with the api

--- ILdbwsClient.cs
+++ ILdbwsClient.cs
@@ -4,5 +4,15 @@ namespace TrainDepartureBoard;
 
 public interface ILdbwsClient
 {
-    Task<GetDepBoardWithDetailsResponse> GetDepBoardWithDetails(int numRows, string crs, int timeWindow, string filterCrs = "", string filterType = "to", int timeOffset = 0);
+    Task<GetArrBoardWithDetailsResponse> GetArrBoardWithDetails(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetArrDepBoardWithDetailsResponse> GetArrDepBoardWithDetails(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetArrivalBoardResponse> GetArrivalBoard(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetArrivalDepartureBoardResponse> GetArrivalDepartureBoard(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetDepartureBoardResponse> GetDepartureBoard(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetDepBoardWithDetailsResponse> GetDepBoardWithDetails(int numRows, string crs, string filterCrs = "", string filterType = "to", int timeOffset = 0, int timeWindow = 120);
+    Task<GetFastestDeparturesResponse> GetFastestDepartures(string crs, List<string> filterCrsList, int timeOffset = 0, int timeWindow = 120);
+    Task<GetFastestDeparturesWithDetailsResponse> GetFastestDeparturesWithDetails(string crs, List<string> filterCrsList, int timeOffset = 0, int timeWindow = 120);
+    Task<GetNextDeparturesResponse> GetNextDepartures(string crs, List<string> filterCrsList, int timeOffset = 0, int timeWindow = 120);
+    Task<GetNextDeparturesWithDetailsResponse> GetNextDeparturesWithDetails(string crs, List<string> filterCrsList, int timeOffset = 0, int timeWindow = 120);
+    Task<GetServiceDetailsResponse> GetServiceDetails(string serviceId);
 }
